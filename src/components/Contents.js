f157e507718f4526fd23bbf356d2b0740b62a0fc
SaiUm <50584754+mommuge@users.noreply.github.com>
2021-10-26 03:04:45 +0900

    makednow

---
+++ src/components/Contents.js
@@ -0,0 +1,14 @@
+import {useState, userEffect} from 'react'
+
+const Contents = () => {
+    return (
+        <section>
+        <h2>국내 코로나 현황</h2>
+        <div className = "contents">
+            
+        </div>
+        </section>
+    )
+}
+
+export default Contents
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="App">
+    <haeder className="header">
+        <h1>COVID-19</h1>
+    <select>
+    <option>국내</option>
+    <option>세계</option>
+    </select>
+    </haeder>
+    </div>
+    )
+}
+
+export default Header
